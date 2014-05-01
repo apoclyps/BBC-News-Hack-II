2a8584217181a3b0fc326182a12d187dd5206ba4
Toby <toby.philp@mac.com>
2014-05-01 18:20:50 +0100

    basic case, need to sort out some url encoding for search term before concat

--- Extension/results/js/app.js
+++ Extension/results/js/app.js
@@ -1,13 +1,10 @@
 App = Ember.Application.create();
-var searchTerm = "";
 
 chrome.extension.onMessage.addListener(function (msg, _, sendResponse) {
     //msg.data is selected text from page
-
     var controller = App.__container__.lookup("controller:index");
     var boundSend = controller.send.bind(controller);
     boundSend('goMain', msg.data);
-
 });
 
 App.Router.map(function () {
@@ -18,41 +15,43 @@ App.Router.map(function () {
 App.IndexController = Ember.ObjectController.extend({
     actions: {
         goMain: function(searchTerm) {
-            /// load
-            // Pass model
             this.transitionToRoute('main');
+            App.set('searchTerm', searchTerm);
         }
     }
 });
 
 App.MainRoute = Ember.Route.extend({
-  model: function() {
-    var searchTerm = App.get('searchTerm');
-    return [
-    {
-        published: "2014-05-01",
-        source: searchTerm,
-        title: "Vladimir Putin calls for Ukrainian troops to withdraw from south-east",
-        description: "In phone call with Angela Merkel, Russian president says military withdrawal and national dialogue are key issues in Ukraine Vladimir Putin has called for Ukrainian troops to pull out of the south-east of the country, in a conversation with Angela Merkel on Thursday. The Russian president said military withdrawal, an end to violence and a national dialogue were the key issues in Ukraine, according to a Kremlin briefing on the phone conversation.   Continue reading...",
-        url: "http://feeds.theguardian.com/c/34708/f/663828/s/39f4eaa1/sc/1/l/0L0Stheguardian0N0Cworld0C20A140Cmay0C0A10Cvladimir0Eputin0Eukraine0Etroops0Ewithdrawal0Eangela0Emerkel/story01.htm",
-        image: "http://static.guim.co.uk/sys-images/Guardian/Pix/pictures/2014/5/1/1398947333984/Ukrainian-troops-near-Slo-011.jpg"
-    },{
-        published: "2014-05-01",
-        source: "TheGuardian",
-        title: "Vladimir Putin calls for Ukrainian troops to withdraw from south-east",
-        description: "In phone call with Angela Merkel, Russian president says military withdrawal and national dialogue are key issues in Ukraine Vladimir Putin has called for Ukrainian troops to pull out of the south-east of the country, in a conversation with Angela Merkel on Thursday. The Russian president said military withdrawal, an end to violence and a national dialogue were the key issues in Ukraine, according to a Kremlin briefing on the phone conversation.   Continue reading...",
-        url: "http://feeds.theguardian.com/c/34708/f/663828/s/39f4eaa1/sc/1/l/0L0Stheguardian0N0Cworld0C20A140Cmay0C0A10Cvladimir0Eputin0Eukraine0Etroops0Ewithdrawal0Eangela0Emerkel/story01.htm",
-        image: "http://static.guim.co.uk/sys-images/Guardian/Pix/pictures/2014/5/1/1398947333984/Ukrainian-troops-near-Slo-011.jpg"
-    },
+    model: function () {
+        var self = this;
 
-    {
-        published: "2014-05-01",
-        source: "TheGuardian",
-        title: "Germany blocks Edward Snowden from testifying in person in NSA inquiry",
-        description: "Officials say a personal invitation for US whistleblower to attend hearing would put 'grave strain' on US-German relations The German government has blocked Edward Snowden from giving personal evidence in front of a parliamentary inquiry into NSA surveillance, it has emerged hours before Angela Merkel travels to Washington for a meeting with Barack Obama. In a letter to members of a parliamentary committee obtained by Sddeutsche Zeitung, government officials say a personal invitation for the ...",
-        url: "http://feeds.theguardian.com/c/34708/f/663828/s/39f3f65c/sc/1/l/0L0Stheguardian0N0Cworld0C20A140Cmay0C0A10Cgermany0Eedward0Esnowden0Ensa0Einquiry/story01.htm",
-        image: "http://static.guim.co.uk/sys-images/Guardian/Pix/pictures/2014/5/1/1398939826137/Edward-Snowden-009.jpg"
+
+        return $.ajax({
+            type: 'GET',
+            url: bbcAPI + App.get('searchTerm'),
+            cache: false,
+            contentType: "application/json",
+            dataType: 'json',
+            crossDomain: true
+        }).then(function(data) {
+             return extract(data);
+        });
     }
-    ];
-  }
 });
+
+var bbcAPI = "http://data.bbc.co.uk/bbcrd-juicer/articles.json?apikey=Qc2qPD1jlgl8Yz9hCokogAToVQ5iOeV8&text=";
+
+function extract(articlesJSON){
+    var newArticles = [];
+    articlesJSON.articles.forEach(function (article, index) {
+        var newArticle = {}
+        newArticle.title = article.title;
+        newArticle.published = article.published;
+        newArticle.description = article.description;
+        newArticle.url = article.url;
+        newArticle.source = article.source;
+        newArticles.push(newArticle);
+    });
+   return newArticles;
+}
+
