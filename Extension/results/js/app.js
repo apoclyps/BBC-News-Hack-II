34e893d541e0ebb1720c91d5aec09e160670f887
Toby <toby.philp@mac.com>
2014-05-02 15:28:14 +0100

    without map reduce and entity recognition

--- Extension/results/js/app.js
+++ Extension/results/js/app.js
@@ -23,47 +23,51 @@ App.IndexController = Ember.ObjectController.extend({
 
 App.MainRoute = Ember.Route.extend({
     model: function () {
-        var self = this;
-		var formData = {
-		    text: App.get('searchTerm'),
-			confidence: 0.2,
-			support: 20
-		};
-
-		return $.ajax({
-            type: 'POST',
-            url: "http://spotlight.dbpedia.org/rest/annotate",
-			data: formData,
-            crossDomain: true
-        }).then(function(data) {
-			var resource = getEntitites(data);
+//         var self = this;
+// 		var formData = {
+// 		    text: App.get('searchTerm'),
+// 			confidence: 0.2,
+// 			support: 20
+// 		};
+//
+// 		return $.ajax({
+//             type: 'POST',
+//             url: "http://spotlight.dbpedia.org/rest/annotate",
+// 			data: formData,
+//             crossDomain: true,
+//             cache: true
+//         }).then(function(data) {
+// 			var entities = getEntitites(data);
 
 		    return $.ajax({
 				type: 'GET',
 				url: bbcAPI + encodeURIComponent('"' + App.get('searchTerm').replace('\s', ' AND ')),
-				cache: false,
+				cache: true,
 				contentType: "application/json",
 				dataType: 'json',
 				crossDomain: true
 			}).then(function(data) {
 				var modelArray = extract(data);
 				modelArray.searchTerm = App.get('searchTerm');
+                // modelArray.forEach(function (article) {
+//                     map( article, entities );
+//                 });
 
 				return modelArray;
 			});
-		});
+// 		});
 	}
 });
 
-App.MainController = Ember.ArrayController.extend({
-    articles: (function () {
-		return Ember.ArrayProxy.createWithMixins(Ember.SortableMixin, {
-			sortProperties: ['keywordCount'],
-			sortAscending: false,
-			content: this.get('content')
-		});
-	} ).property('content')
-});
+// App.MainController = Ember.ArrayController.extend({
+//     articles: (function () {
+// 		return Ember.ArrayProxy.createWithMixins(Ember.SortableMixin, {
+// 			sortProperties: ['keywordCount'],
+// 			sortAscending: false,
+// 			content: this.get('content')
+// 		});
+// 	} ).property('content')
+// });
 
 var bbcAPI = "http://data.bbc.co.uk/bbcrd-juicer/articles.json?apikey=Qc2qPD1jlgl8Yz9hCokogAToVQ5iOeV8&text=";
 
@@ -78,7 +82,6 @@ function extract(articlesJSON){
         newArticle.url = article.url;
         newArticle.source = article.source;
 
-        newArticle.keywordCount = index % 10;
 
         if (article.image) {
             newArticle.image = article.image.src;
@@ -102,4 +105,129 @@ function getEntitites(data) {
 }
 
 
+function map(article,entities){
+				var articleWords = getArticleRanking(article);
+				article.keywordCount = getArticleRelevanceScore(articleWords,entities);
+			}
 
+			function getArticleRelevanceScore(articleWords,entities){
+				var overallRelevance = 0;
+
+				entities.forEach(function (word, wordIndex){
+					console.log(word);
+					articleWords.forEach(function (match, matchIndex){
+						console.log(match);
+						if(word.toLowerCase()==match.key.toLowerCase()){
+							overallRelevance++;
+						}
+					});
+				});
+				return overallRelevance;
+			}
+
+			function getArticleRanking(article)
+			{
+				var data = extractArticleDescriptions(article);
+				var wordsArray = splitArticleWords(data);
+				var results = sort(wordsArray);
+				display(results);
+
+				return results;
+			}
+
+			function extractArticleDescriptions(article) {
+			    var descriptionList = [];
+			    var word = article.description.toLowerCase().replace(/[^a-zA-Z0-9]/g, ' ');
+			    descriptionList.push(word)
+			    return descriptionList;
+			}
+
+			function splitArticleWords(data) {
+			    var articleWords = [];
+
+			    var arr = [];
+			    arr = data[0].split(" ");
+
+			    arr.forEach(function (word, wIndex) {
+			        if (word.length >= 4) {
+			            articleWords.push(word);
+			        }
+			    });
+			    return articleWords;
+			}
+
+			function splitWords() {
+			    var articleWords = [];
+			    data.forEach(function (description, dIndex) {
+			        var arr = [];
+			        arr = description.split(" ");
+			        arr.forEach(function (word, wIndex) {
+			            if (word.length >= 4) {
+			                articleWords.push(word);
+			            }
+			        });
+			    });
+			    return articleWords;
+			}
+
+			function sort(wordsArray) {
+			    var result = mapreduce(wordsArray, function (item, emit) {
+			        var splitted = item.split(/\s/g);
+			        for (var word in splitted) {
+			            emit(splitted[word], 1);
+			        }
+			    }, function (key, values, emit) {
+			        var length;
+			        if ( values )  {
+			            length = values.length;
+			        } else {
+			            length = 0;
+			        }
+			        emit({
+			            key: key,
+			            count: length
+			        });
+			    });
+
+			    result.sort(function (a, b) {
+			        return b.count - a.count;
+			    });
+			    return result;
+			}
+
+			function display(result) {
+			    // all items that were emitted in the reduce step are now put into the 'result' variable
+			    // and we can iterate over this collection
+			    for (var ix = 0; ix < result.length; ix++) {
+			        // we have created objects in the form { key, count }
+			        // and we can write this to the screen
+			        document.write(result[ix].key + ': ' + result[ix].count + '<br/>');
+			    }
+			}
+
+			//============================================================================
+			// 				MAP Reduce Functions for All Articles
+			//============================================================================
+			function extractDescriptions(articlesJSON) {
+			    var articles = articlesJSON[0].articles;
+			    var descriptionList = [];
+			    for (var i in articlesJSON[0].articles) {
+			        var word = articles[i].description.toLowerCase().replace(/[^a-zA-Z0-9]/g, ' ');
+			        descriptionList.push(word);
+			    }
+			    return descriptionList;
+			}
+
+			function splitWordsArticles() {
+			    var articleWords = [];
+			    data.forEach(function (description, dIndex) {
+			        var arr = [];
+			        arr = description.split(" ");
+			        arr.forEach(function (word, wIndex) {
+			            if (word.length >= 4) {
+			                articleWords.push(word);
+			            }
+			        });
+			    });
+			    return articleWords;
+			}
